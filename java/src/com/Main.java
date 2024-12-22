59cdb9742e3f73fcb5b2c870c181d9bb07f2d9c3
Ruben Contreras <4tipruben@gmail.com>
2024-12-22 16:52:02 -0300

    inciio curso

---
+++ java/src/com/Main.java
@@ -0,0 +1,22 @@
+package com;
+
+public class Main {
+    public static void main(String[] args) {
+        //  PRIMITIVOS
+        int number1 = 20;
+        int number2 = 20;
+        int result = number1 + number2;
+        System.out.println(result);
+        double pi = 3.14159265358979;
+        boolean isAdult = false;
+        char a = 'A';
+        // Guiones bajos para visibilizar mejor los numeros declarados
+        // Sirve con varibels decimales tambien
+        int amount = 1_000_000_008;
+        
+
+
+
+
+    }
+}
